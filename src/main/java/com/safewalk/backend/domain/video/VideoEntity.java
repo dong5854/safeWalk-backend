d530a841ceedfd861a8046bdef7037bb7a0d0aa1
dong young <dong5854@gmail.com>
2022-08-20 03:24:42 +0900

    feat: add fileUUID

--- src/main/java/com/safewalk/backend/domain/video/VideoEntity.java
+++ src/main/java/com/safewalk/backend/domain/video/VideoEntity.java
@@ -1,16 +1,19 @@
 package com.safewalk.backend.domain.video;
 
-import lombok.Getter;
-import lombok.ToString;
+import lombok.*;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity @Getter @ToString
 @Table(name = "video")
 public class VideoEntity {
     @Id
-    private String filename;
+    private String fileUUID;
     private String filepath;
+    private String originalFilename;
 }
+
