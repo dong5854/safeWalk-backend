da41569804727fea1733c301e4442c78c3387872
dong young <dong5854@gmail.com>
2022-08-20 03:37:00 +0900

    feat: add find service

--- src/main/java/com/safewalk/backend/domain/video/StoreService.java
+++ src/main/java/com/safewalk/backend/domain/video/StoreService.java
@@ -3,31 +3,43 @@ package com.safewalk.backend.domain.video;
 import com.safewalk.backend.config.StorageProperties;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
 public class StoreService {
 
     private final StorageProperties storageProperties;
+    private final VideoRepository videoRepository;
 
+    @Transactional
     public String store(MultipartFile file) {
+        String fileUUID = UUID.randomUUID().toString();
+        String filePath = storageProperties.getLocation() + fileUUID;
+        VideoEntity videoEntity = new VideoEntity(fileUUID, filePath, file.getOriginalFilename());
         try {
-            file.transferTo(new File(storageProperties.getLocation() + file.getOriginalFilename()));
-            System.out.println(storageProperties.getLocation() + file.getOriginalFilename());
-        } catch (IOException e){
-            e.printStackTrace();
-            System.out.println(e);
-            return "저장 실패";
-        } catch (IllegalStateException e){
+            videoRepository.save(videoEntity);
+            file.transferTo(new File(storageProperties.getLocation() + fileUUID));
+        } catch (IOException | IllegalStateException e){
             e.printStackTrace();
             System.out.println(e);
             return "저장 실패";
         }
 
-        return file.getOriginalFilename() + " 저장 성공";
+        return file.getOriginalFilename() + " saved as " + fileUUID;
+    }
+
+    public List<VideoEntity> findByOriginalName(String originalName){
+        return videoRepository.findVideoEntitiesByOriginalFilenameIgnoreCase(originalName);
+    }
+
+    public VideoEntity findByUUID(String fileUUID){
+        return videoRepository.findVideoEntitiesByFileUUID(fileUUID);
     }
 }
